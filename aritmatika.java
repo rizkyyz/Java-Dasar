b679a0697fdf76ee8181bff60869527ac204ed19
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-04-02 03:17:47 +0700

    Create aritmatika.java

---
+++ aritmatika.java
@@ -0,0 +1,28 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      // BELAJAR ARITMATIKA 
+     int variable1 = 9;
+     int variable2 = 3;
+     
+     int hasil;
+    
+    // PENJUMLAHAN
+    hasil = variable1 + variable2;
+    System.out.println (variable1 + " + " + variable2 + " = " + hasil);
+    
+    //PENGURANGAN
+    hasil = variable1 - variable2;
+    System.out.printf ("%d - %d = %d \n",variable1,variable2,hasil);
+    
+    //PERKALIAN
+    hasil = variable1 * variable2;
+    System.out.printf ("%d x %d = %d \n",variable1,variable2,hasil);
+    
+    //PEMBAGIAN
+     hasil = variable1 / variable2;
+    System.out.printf ("%d / %d = %d \n",variable1,variable2,hasil);
+    
+    //MODULUS
+     hasil = variable1 % variable2;
+     System.out.printf ("%d %% %d = %d \n",variable1,variable2,hasil);
+     
