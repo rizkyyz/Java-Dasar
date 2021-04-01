1cb0aea47e86fe5ffbe90d1a1c5e89cb604e1262
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-04-02 02:30:30 +0700

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println("jadi nilai a = " + a); 
+        
+        a = 200;
+        System.out.println ("jadi nilai baru a = " + a);
+        //rizkyyz
+        //mendeklarasi b
+        int b;
+        b  = 2102;
+        System.out.println ("deklarasi dari b = " + b);
+    }
+}
