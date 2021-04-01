be9c2200bb45db5437764f973a45b74f7f43388c
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-04-02 02:22:18 +0700

    Create pengertianjava.java

---
+++ pengertianjava.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        //println
+       System.out.println ("hallo sayang ");
+       //PRINT
+       System.out.print ("saya rizky ");
+       System.out.print ("ganteng banget \n");
+       //PRINTF
+        System.out.printf ("umur %d", 18);       
+    }
+}
